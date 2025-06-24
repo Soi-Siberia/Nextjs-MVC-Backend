7c611d6e4f14d4ed9a0acbe91cea3e49502366f6
Soi-Siberia <hungvn230723@gmail.com>
2025-06-24 13:33:39 +0700

    6.6 xong add user qua api vào db

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UsersService {
   // create(createUserDto: CreateUserDto) {
   //   return 'This action adds a new user';
   // }
-  create(name: string, mail: string, password: string) {
-    return `This action adds a new user with name: ${name}, mail: ${mail}, password: ${password}`;
+
+  constructor(@InjectModel(User.name) private UserModel: Model<User>) { }
+
+  async create(name: string, mail: string, password: string) {
+    let user = await this.UserModel.create({
+      name, mail, password
+    })
+    return user;
   }
+  // return `This action adds a new user with name: ${name}, mail: ${mail}, password: ${password}`;
 
   findAll() {
     return `This action returns all users`;
