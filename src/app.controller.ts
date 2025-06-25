d51916daf76582d0bf894822fba893360c26858c
Soi-Siberia <hungvn230723@gmail.com>
2025-06-25 15:39:36 +0700

    guard tuong duong middewre, hoafn thanh login

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Post, Render, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config'; // Import ConfigService to access environment variables
+import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
@@ -9,12 +11,15 @@ export class AppController {
     private configService: ConfigService, // Import ConfigService to access environment variables
   ) { }
 
-  @Get()
-  @Render("home")
-  handleHompage() {
-    const message = this.appService.getHello();
-    return {
-      message: message
-    };
+  // @UseGuards(AuthGuard('local')) //hasd code to be used with the local strategy
+  // @Post('/login')
+  // handleLogin(@Request() req): string {
+  //   return req.user; // Return the user object after successful login
+  // }
+
+  @UseGuards(LocalAuthGuard) //hasd code to be used with the local strategy
+  @Post('/login')
+  handleLogin(@Request() req): string {
+    return req.user; // Return the user object after successful login
   }
 }
